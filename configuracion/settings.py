be40888cd7f69c3c48006a7ff236021724fc1cbe
rodrigomedrano-jpg <rodrigo.medrano@itsecsucre.edu.bo>
2026-03-18 15:47:25 -0400

    Templates en curso

--- configuracion/settings.py
+++ configuracion/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'inicio',
+    'cursos',
 ]
 
 MIDDLEWARE = [
@@ -114,9 +116,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Agregar esta línea justo debajo (si no existe)
+STATICFILES_DIRS = []  # Para desarrollo
+
+# Para producción (agregar al final)
+STATIC_ROOT = BASE_DIR / 'staticfiles'
